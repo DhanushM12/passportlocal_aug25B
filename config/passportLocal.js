757a38c652219566db2203112b6103e67380c69c
DhanushM12 <dhanushmukhi@gmail.com>
2023-03-26 14:01:52 +0530

    added config

---
+++ config/passportLocal.js
@@ -0,0 +1,31 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail, getUserByID){
+    const authenticator = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (!user) { // with this email no user exists
+             return done(null, false); 
+        }
+        try {
+            if(await bcrypt.compare(password, user.password)) {// user is valid
+                return done(null, user);
+            }
+            else{// passwords didn't match
+                return done(null, false);
+            }
+        } catch (error) {
+            return done(error)
+        }
+    }
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticator ));
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+            done((null, getUserByID(id)))
+      });
+}
+
+module.exports = initialize;
