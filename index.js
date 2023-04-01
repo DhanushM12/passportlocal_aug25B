4f1618ce6b26c65921b6ceb564367fb6c24d1eb3
DhanushM12 <dhanushmukhi@gmail.com>
2023-04-01 12:57:59 +0530

    Added ejs and middleware

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const express = require('express')
 const app= express();
 const port = 8080;
+const passport = require('passport')
+const session = require('express-session')
+const initialize = require('./config/passportLocal')
+
+const users = [];
+
+initialize(passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id))
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index')
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login')
+})
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register')
+})
+
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    return res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    return next()
+}
 
 app.listen(port, function(err){
     if(err){
